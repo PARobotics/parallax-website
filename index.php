e965157594ccd685c946d311d9b7ca8016ca063d
jeffreyshen19 <jshen@andover.edu>
2017-09-09 21:59:17 -0400

    update header

--- index.php
+++ index.php
@@ -1,1 +1,8 @@
 <?php header( 'Location: /index.html' ) ;  ?>
+<head>
+<meta charset = "utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name = "description" content = "Homepage for Team 8900 Parallax, Phillips Academy's competitive robotics team">
+<meta name = "author" content = "Jeffrey Shen">
+</head>
